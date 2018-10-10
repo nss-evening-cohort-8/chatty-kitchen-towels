abe5110c53153f9acb422854198d32fb8f4e8a2f
Wayne Collier <ke4tri@gmail.com>
2018-10-10 06:10:22 -0500

    added string file to place all the string building code

--- javascripts/components/string.js
+++ javascripts/components/string.js
@@ -3,7 +3,6 @@ import {getMessages} from "../Data/data.js"
 let choices = [];
 
 const setCharacters = (newArray) => {
-  console.log('setCharacters');  
   choices = newArray;
   console.log(choices);
   console.log(newArray);

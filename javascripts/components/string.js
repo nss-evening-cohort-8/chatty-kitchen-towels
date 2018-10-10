186ae0c6f25d532fedbc6d9e1993b261e9a6b829
Wayne Collier <ke4tri@gmail.com>
2018-10-10 06:30:05 -0500

    added print to dom to util.js file and started string builder

--- javascripts/components/string.js
+++ javascripts/components/string.js
@@ -8,4 +8,26 @@ const setCharacters = (newArray) => {
   console.log(newArray);
   };
 
+
+  const detailsBuilder = (messagesArray) => {
+    let domString = '';
+    messagesArray.forEach((message) => { //this turns pets/getpetz into a new array character
+       domString +=   `<div class="card  mb-3 bg-light" style="width: 18rem;">`
+       domString +=   `<div class="card-body ">`
+       domString +=       `<h3 class="card-link text-center">${message.name}</h3>`
+       domString +=   `</div>`
+       domString +=   `<div class="card-body ">`
+       domString +=       `<h5 class="card-title text-center">${message.color}</h5>`
+       domString +=       `<p class="card-title text-center">${message.specialSkill}</p>`
+       domString +=   `</div>`
+       domString +=   `<div class="card-body ">`
+       domString +=       `<h5 class="card-link text-center" id="${message.type}">${message.type}</h5>`
+       domString +=   `</div>`
+       domString +=   `</div>` 
+    });
+    printToDom(domString);
+    // sortEvents();
+  };
+
+
   export {setCharacters}

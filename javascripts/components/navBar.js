736d28453b01a7dc54fe49106e8cb316a9c3f22f
Ripal Patel <ripal212007@gmail.com>
2018-10-10 19:52:26 -0500

    still not working

--- javascripts/components/navBar.js
+++ javascripts/components/navBar.js
@@ -1,13 +1,28 @@
 import{choices} from "./string.js";
 
+
+
+let messageBox = document.getElementById('message-box');
+const users = document.getElementById('names');
+
+const selectUsers =()=>{
+const userName = document.querySelector('.user:checked').value;
+console.log(userName);
+}
+selectUsers();
+    
+
 const inputString = () => {
     const userInput = document.getElementById('input-field');
-    let messageBox = document.getElementById('message-box');
-    inputField.addEventListener('keypress', (e)=> {
+    userInput.addEventListener('keypress', (e)=> {
         if(e.keyCode===13){
-          messageBox.innerHTML = userInput.value;
-        
+            console.log('u hit enter');
+        //   messageBox.innerHTML = userInput.value;
         }
     })
 }
 inputString();
+
+// const getmessagez = ()=>{
+//     return messageaArray;
+// }

e82f1cdac463ebebca0727ce1913eb3baf737857
Wayne Collier <ke4tri@gmail.com>
2018-10-12 16:27:45 -0500

    commiting before checking another branch

--- javascripts/components/edit.js
+++ javascripts/components/edit.js
@@ -8,25 +8,25 @@
 //     })
 //     };
 
-const messEdit = () => {
-    // const editButtons = document.getElementsByClassName('newMessage1');
-    // const editButtons =
-     document.getElementById('message11').addEventListener("click",function(){
-         console.log("messEdit Prints")
-     });
-
-    // console.log(editButtons);
-    // for (let i = 0; i < editButtons.length; i++) {
-    //     const element = editButtons[i];
-    //         element.addEventListener("click", (e) => {
+// const messEdit = () => {
+//     const editButtons = document.getElementsByClassName("newMessage1");
+    
+//     editButtons.addEventListener("click",console.log("messEdit Prints"))
+         
+//     };
         
-    //         const buttonIClicked = e.target;
-    //         const cardToDelete = buttonIClicked.parentNode.parentNode;
-    //         cardToDelete.remove();
-    //         })
-    //  }
-    }
-    // document.getElementById("message11").addEventListener("click", function(){messEdit()});
+    const messEdit = () => {
+        const editButtons = document.getElementsByClassName("newMessage1");
+        console.log('editButtons');
+        for (let i = 0; i < editButtons.length; i++) {
+          const element = editButtons[i];
+          element.addEventListener("click", (e) => {
+            // const buttonIClicked = e.target;
+            // const cardToDelete = buttonIClicked.parentNode.parentNode;
+            // cardToDelete.remove();
+          })
+        }
+      }
 
 
 export {messEdit}

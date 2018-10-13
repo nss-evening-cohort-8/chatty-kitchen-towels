efa9266118d7796e5b5e1dcef03b0125bab0c8ce
Wayne Collier <ke4tri@gmail.com>
2018-10-13 11:39:54 -0500

    Delete buttons working

--- javascripts/components/delete.js
+++ javascripts/components/delete.js
@@ -1,13 +1,51 @@
+import {choices} from "../components/string.js"
+import {detailsBuilder} from "../components/string.js"
+
 const activateDeletes = () => {
-    const deleteButtons = document.getElementsByClassName('newMessage2');
-    for (let i = 0; i < deleteButtons.length; i++) {
-      const element = deleteButtons[i];
-      element.addEventListener("click", (e) => {
-        const buttonIClicked = e.target;
-        const cardToDelete = buttonIClicked.parentNode.parentNode;
-        cardToDelete.remove();
-      })
-    }
+  const deleteButtons = document.getElementsByClassName('newMessage2');
+  for (let i = 0; i < deleteButtons.length; i++) {
+    const element = deleteButtons[i];
+    element.addEventListener("click", (e) => {
+      console.log("Delete Buttons are active")
+      for(let i=0;i<choices.length;i++){
+        console.log(choices[i]);
+        if(choices[i]["message"] + " " + choices[i]["createdAt"] === e.target.parentElement.parentElement.childNodes[1].firstChild.innerText){
+          // delete choices[i];
+          choices.splice(i, 1);
+          detailsBuilder(choices);
+          // activateDeletes();
+        }
+        
+      }
+    
+    })
   }
+}
+// choices[i]["message"] + " " + choices[i]["createdAt"]
+
+// const activateDeletes = () => {
+//     const deleteButtons = document.getElementsByClassName('newMessage2');
+//     for (let i = 0; i < deleteButtons.length; i++) {
+//       const element = deleteButtons[i];
+//       element.addEventListener("click", (e) => {
+//         const buttonIClicked = e.target;
+//         const cardToDelete = buttonIClicked.parentNode.parentNode;
+//         cardToDelete.remove();
+//       })
+//     }
+//   }
+
+// const activateDeletes = () => {
+//   const deleteButtons = document.getElementsByClassName('newMessage2');
+//   for (let i = 0; i < deleteButtons.length; i++) {
+//     const element = deleteButtons[i];
+//     element.addEventListener("click", (e) => {
+//       for(i=0;i<choices.length;i++){
+ //       if(
+//choices[i]["message"] === e.target.parentElement.parentElement.childNodes[1].firstChild.innerText){
+//          delete choices[i]
+//           console.log("delete is working");
+//         }
+//       }
 
 export {activateDeletes}

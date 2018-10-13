1ac524fde62bb57d8830ec7044b0f4c5434f48bf
Wayne Collier <ke4tri@gmail.com>
2018-10-13 11:47:34 -0500

    changed some spacing

--- javascripts/components/delete.js
+++ javascripts/components/delete.js
@@ -10,10 +10,8 @@ const activateDeletes = () => {
       for(let i=0;i<choices.length;i++){
         console.log(choices[i]);
         if(choices[i]["message"] + " " + choices[i]["createdAt"] === e.target.parentElement.parentElement.childNodes[1].firstChild.innerText){
-          // delete choices[i];
           choices.splice(i, 1);
           detailsBuilder(choices);
-          // activateDeletes();
         }
         
       }
@@ -21,31 +19,6 @@ const activateDeletes = () => {
     })
   }
 }
-// choices[i]["message"] + " " + choices[i]["createdAt"]
 
-// const activateDeletes = () => {
-//     const deleteButtons = document.getElementsByClassName('newMessage2');
-//     for (let i = 0; i < deleteButtons.length; i++) {
-//       const element = deleteButtons[i];
-//       element.addEventListener("click", (e) => {
-//         const buttonIClicked = e.target;
-//         const cardToDelete = buttonIClicked.parentNode.parentNode;
-//         cardToDelete.remove();
-//       })
-//     }
-//   }
-
-// const activateDeletes = () => {
-//   const deleteButtons = document.getElementsByClassName('newMessage2');
-//   for (let i = 0; i < deleteButtons.length; i++) {
-//     const element = deleteButtons[i];
-//     element.addEventListener("click", (e) => {
-//       for(i=0;i<choices.length;i++){
- //       if(
-//choices[i]["message"] === e.target.parentElement.parentElement.childNodes[1].firstChild.innerText){
-//          delete choices[i]
-//           console.log("delete is working");
-//         }
-//       }
 
 export {activateDeletes}

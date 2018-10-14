f8bffbdb95f4434f17117d96a972419af3e5a071
Wayne Collier <ke4tri@gmail.com>
2018-10-14 18:59:41 -0500

    got to the point of building new string

--- javascripts/components/edit.js
+++ javascripts/components/edit.js
@@ -19,6 +19,15 @@ const messEdit = () => {
   }
 }
 
+
+// let messIdClean = (newCleanMessId) => {
+//   console.log(newCleanMessId);
+//   let newCleanMessId2 = `"${newCleanMessId.substring(0, newCleanMessId.length-1)}"`;
+//  "${newCleanMessId2}"`;
+  // let newCleanMessId3 = newCleanMessId2.toString();
+  
+
+
 const clearInput = () => {
    document.getElementById("input-field").value = '';
   document.getElementById("input-field").blur();
@@ -40,14 +49,34 @@ let findElement = () => {
   let gutsEqual = event.target.value;
   let userEdit = window.value.parentElement.parentElement.childNodes[0].childNodes[0].innerHTML;
   let message_id = window.value.parentElement.parentElement.childNodes[1].childNodes[0].id
+  // let thisMessageId = messIdClean(message_id);
+  let newCleanMessId2 = `${message_id.substring(0, message_id.length-1)}`;
   let createdMessage = { messageId: message_id, name: userEdit, message: gutsEqual, createdAt: moment().calendar() };
-  let i=0;
-    for(i=0;i<choices;i++){
-      if(choices[i].messageId === message_id){
-      console.log(choices[i]);
-      };
-    };
+  
+  skimFunction(newCleanMessId2);
+  return message_id,createdMessage;
+  };
+
+  let skimFunction = (varHere) => {
+  choices.forEach((choice) =>{
+    let oldId = choice["messageId"];
+    if(oldId === varHere){
+      console.log("match hthis");
+    }
+  });
+ 
+
+
+  // let i=0;
+  //   for(i=0;i<choices;i++){
+  //     if(choices[i].messageId === message_id){
+  //     console.log(choices[i]);
+  //     };
+  //   };
 
 };
 
 export { messEdit, findElement}
+
+
+
